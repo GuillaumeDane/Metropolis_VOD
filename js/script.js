408d79aa33d245361b48b3f0e91f1783a4884ae7
GuillaumeDane <daneguillaume.prod@gmail.com>
2018-07-12 10:44:20 +0200

    +

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Parallax materialize
+
 $(document).ready(function(){
    $('.parallax').parallax();
  });
+
+// Show form
+
+$('#login').click(function(){
+      $('#form1').toggle();
+      });
+
+  $('#button-cree').click(function(){
+        $('#form2').toggle();
+        });
