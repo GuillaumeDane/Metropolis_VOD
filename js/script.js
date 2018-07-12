fbb594080289ca420a25dce8da490e4d071726f0
GuillaumeDane <daneguillaume.prod@gmail.com>
2018-07-12 12:26:36 +0200

    +

--- js/script.js
+++ js/script.js
@@ -10,6 +10,12 @@ $('#login').click(function(){
       $('#form1').toggle();
       });
 
-  $('#button-cree').click(function(){
-        $('#form2').toggle();
-        });
+$('#button-cree').click(function(){
+      $('#form2').toggle();
+      });
+
+// Hamburger
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
